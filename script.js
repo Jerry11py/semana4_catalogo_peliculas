d5c9a5562014354b703a7efa5bc5fe6bfaa5f425
Jerry11py <jerry10py@gmail>
2024-10-08 17:05:32 -0300

    Cambios en funcion displayMovies

--- script.js
+++ script.js
@@ -29,13 +29,13 @@ async function fetchPopularMovies() {
 function displayMovies(movies) {
     movieList.innerHTML = ''; // Limpia la lista de películas
     movies.forEach(movie => {
-        const li = document.createElement('li');
-        li.innerHTML = `
+        const imgContainer = document.createElement('div');
+        imgContainer.innerHTML = `
             <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
-            <span>${movie.title}</span>
+            <p>${movie.title}</p>
         `;
-        li.onclick = () => showMovieDetails(movie.id); // Muestra detalles al hacer clic en la película
-        movieList.appendChild(li);
+        imgContainer.onclick = () => showMovieDetails(movie.id); // Muestra detalles al hacer clic en la película
+        movieList.appendChild(imgContainer);
     });
 }
 
